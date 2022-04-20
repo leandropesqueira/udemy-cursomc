a5aa8d5e6068e6b03a41c6720e578e8327f11596
leandropesqueira <leandropesqueira7@gmail.com>
2022-04-20 17:38:28 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/leandrolopes/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/leandrolopes/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.leandrolopes.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.leandrolopes.cursomc.domain.Categoria;
 
 import lombok.Getter;
@@ -15,7 +19,10 @@ public class CategoriaDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;		
+	private Long id;
+	
+	@NotEmpty(message = "Preenchimento é obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO (Categoria obj) {
--- src/main/java/com/leandrolopes/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrolopes/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leandrolopes.cursomc.domain.Categoria;
+import com.leandrolopes.cursomc.dto.CategoriaDTO;
 import com.leandrolopes.cursomc.repositories.CategoriaRepository;
 import com.leandrolopes.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrolopes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
